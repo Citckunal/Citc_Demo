d619574104e725f477cbcb33d340478a1393d2cc
Citckunal <kunal.g15@gmail.com>
2026-04-23 17:08:36 +0530

    Implement MongoDB connectivity in main.py
    Added MongoDB connectivity for saving and retrieving user data.

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import streamlit as st
-st.title("Welcome In Python Web World")
-age=st.slider("Age",18,80)
-diet=st.selectbox("Diet Quality",['Poor','Avg','Good','Excelent'])
-exercise=st.slider("Exersice Day Per Week",0,7,3)
-stress=st.selectbox("Stress Lavel",["Low","Medium","High"])
-bmi=st.number_input("BMI",10.0,40.0,22.0)
-alcohol=st.selectbox("Alcohol Consumption",['Low','Medium','High'])
-smoking=st.selectbox("Smoking",['Low','Medium','High'])
-family_history=st.selectbox("Family History",["Yes","No"])
-if st.button("Predict Risk"):
-       input_data=[age,diet,exercise,stress,bmi,alcohol,smoking,family_history]
-       st.success(input_data)
+import pymongo
+st.title("DATABASE CONNECTIVITY USING MOGODB")
+c1,c2=st.columns(2)
+
+myclient = pymongo.MongoClient("mongodb+srv://kunalg15_db_user:UchdhJoflEo7GMli@citcapp.mong26u.mongodb.net/?appName=CitcApp")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+n=c1.text_input("Enter name")
+a=c1.text_input("Enter address")
+b=c1.button("Save")
+if b:
+       mydict = { "name":n,"address":a }
+       mycol.insert_one(mydict)
+       c1.success("Record Save")
+
+n1=c2.text_input("Enter name","u")
+a1=c2.text_input("Enter address","a")
+b1=c2.button("Login")
+if b1:
+     res=mycol.find({"name":n1,"address":a1})
+    
+     for data in res:
+             c2.success(f"Welcome:{data['name']}")
+            
